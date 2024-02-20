3afdf8bc14900fbcdcf9b61ad64914cdd46009f6
Multi <83913997+multicolor-rgb@users.noreply.github.com>
2024-02-20 11:57:29 +0100

    cookies plugin with PL added and small edit
    Hidden text area (no working on previous version, no have idea what for is)
    - added pl lang

---
+++ cbcookies/lang/pl_PL.php
@@ -0,0 +1,29 @@
+<?php
+//set internationalization
+
+$i18n = array(
+        "name" => 'cbCookies',
+        "message0" =>  'Używamy plików cookie naszych własnych lub stron trzecich, aby poprawić jakość przeglądania Internetu. Jeśli będziesz kontynuować przeglądanie, uznamy, że akceptujesz ich użycie.',
+        "message1" =>  'Korzystając z tej witryny, wyrażasz zgodę na używanie plików cookie w celach analitycznych, dostosowywania treści i reklam.',
+        "message2" =>  'Ta strona używa COOKIES. Korzystając z niej wyrażasz zgodę na wykorzystywanie cookies, zgodnie z ustawieniami Twojej przeglądarki.',
+        "accept" => 'Akceptuję',
+        "accept1" => 'X',
+        "more_in" => 'Więcej Informacji',
+
+        //BACKEND
+        "setting" => 'CBCOOKIES Ustawienia',
+        "page_more" => 'Informacja do wyświetlenia',
+        "message_lb" => 'Wiadomość o akceptacji plików cookie',
+        "pos_message" => 'Pozycja wiadomości na stronie',
+        "time_cookie" => 'Pliki cookie wygasają po',
+        "cbtop" => 'Góra',
+        "cbbottom" => 'Dół',
+        "cbscale" => 'Units of Time',
+        "cbdays" => 'dni',
+        "cbhours" => 'godzin',
+        "cbminutes" => 'minut',
+        "cbseconds" => 'sekund',
+        "cbSave" => 'Zapisz',
+        "cbchsav" => 'Ustawienia zapisane'
+
+);
